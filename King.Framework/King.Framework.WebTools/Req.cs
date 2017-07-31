5bcba00ef1bbab33da2eaa1d89fa66b286151401
wangdaguo68 <474208445@qq.com>
2017-07-31 09:58:40 +0800

    last commit

--- King.Framework/King.Framework.WebTools/Req.cs
+++ King.Framework/King.Framework.WebTools/Req.cs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-﻿namespace King.Framework.WebTools
-=======
-﻿namespace Drision.Framework.WebTools
->>>>>>> 2b573943baeab4164300c7a14b5c182ddccdbe59
+namespace King.Framework.WebTools
 {
     using System;
 
--- King.Framework/King.Framework.WebTools/ServerInfo.cs
+++ King.Framework/King.Framework.WebTools/ServerInfo.cs
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 ﻿namespace King.Framework.WebTools
-=======
-﻿namespace Drision.Framework.WebTools
->>>>>>> 2b573943baeab4164300c7a14b5c182ddccdbe59
+
 {
     using System;
     using System.Text.RegularExpressions;
